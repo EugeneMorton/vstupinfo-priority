cdbcb27f47816b137787c3805fdeb98c92d4188d
EugeneMorton <EugeneMorton@users.noreply.github.com>
2017-08-03 12:47:36 +0300

    Features: counts amount of prioritys before you

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Ссылка на конкурс";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(470, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(451, 227);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(259, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Инициалы";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(470, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Статистика";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(473, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 211);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ожидание";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 278);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(textBox1.Text, @"http:\/\/vstup\.info\/2017\/.+$"))
+            {
+                MessageBox.Show("Введён некорректный адрес","Ошибка",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                WebClient client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+                string vstupsite = client.DownloadString(textBox1.Text);
+                vstupsite = Regex.Split(vstupsite, @">\s<tbody>")[1];
+                vstupsite = Regex.Split(vstupsite, @"<\/td><\/tr>\s<\/table>")[0];
+                DataSet1 vstup = new DataSet1();
+                List<string> vstrow = new List<string>();
+                var matches = Regex.Matches(vstupsite, @"<tr><td>(\d+)<\/td><td>(.+ .\. .\.)<\/td><td>Допущено<\/td><td>(\d)<\/td><td>(\d+\.\d+)");
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    try { vstup.DataTable1.Rows.Add(new object[] { int.Parse(matches[i].Groups[1].ToString()), matches[i].Groups[2], int.Parse(matches[i].Groups[3].ToString()), matches[i].Groups[4] }); }
+                    catch { }
+                }
+                dataGridView1.DataSource = vstup.DataTable1;
+                dataGridView1.Refresh();
+                if (!Regex.IsMatch(textBox2.Text, @"\S+\s.\.\s.\."))
+                {
+                    label2.Text = "Not available | Некорректное имя";
+                }
+                else if (vstup.DataTable1.Rows.Find(textBox2.Text) == null)
+                {
+                    label2.Text = "Not available | Имени нет в списке";
+                }
+                short first = 0, second = 0, third = 0, forth = 0, fifth = 0, sixth = 0, seventh = 0, eighth = 0, ninth = 0;
+
+                for (int i = 0; i < int.Parse(vstup.DataTable1.Rows.Find(textBox2.Text)[0].ToString()); i++)
+                {
+                    switch (vstup.DataTable1.Rows[i][2])
+                    {
+                        case 1: first++; continue;
+                        case 2: second++; continue;
+                        case 3: third++; continue;
+                        case 4: forth++; continue;
+                        case 5: fifth++; continue;
+                        case 6: sixth++; continue;
+                        case 7: seventh++; continue;
+                        case 8: eighth++; continue;
+                        case 9: ninth++; continue;
+                    }
+                }
+                label2.Text = "До вас: \nПервых приоритетов - " + first + "\nВторых приоритетов - " + second + "\nТретьих приоритетов - " + third + "\nЧетвёртых приоритетов - " + forth + "\nПятых приоритетов - " + fifth + "\nШестых приоритетов - " + sixth + "\nСедьмых приоритетов - " + seventh + "\nВосьмых приоритетов - " + eighth + "\nДевятых приоритетов - " + ninth;
+                //float.Parse(vstup.DataTable1.Rows[5][3].ToString(), System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
